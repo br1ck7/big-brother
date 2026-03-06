aa3e768d9e3bc3d9dc53bc27fbab6e72ec4d9f70
Veikko Kapiya <veikko.kapiya@gmail.com>
2026-03-06 04:17:41 +0200

    responsive ip addresses are added to a list

--- scanner.py
+++ scanner.py
@@ -4,9 +4,16 @@ from config import NETWORK_RANGE
 
 def scan_network(network_range):
     network = ipaddress.ip_network(network_range)
+    address_list = []
+
     for ip in network.hosts():
         result = ping(str(ip))
         if result:
             print(f"{ip} responded in {result} seconds")
+            address_list.append(ip)
 
-scan_network(NETWORK_RANGE)
+    return address_list
+
+scan_result = scan_network(NETWORK_RANGE)
+
+print(scan_result)
